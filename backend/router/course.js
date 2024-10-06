62cd31a05d922e59aae147e1540b947f03ba6d77
Avinash Kushwaha <avinash.kushwaha1501gmail.com>
2024-10-06 16:07:01 +0530

    Updated purchase routes.

--- backend/router/course.js
+++ backend/router/course.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const courseRouter = express.Router();
 const { authMiddleware, userAuthMiddleware } = require('../middlewares/authMiddleware');
-const { courseModel } = require('../database');
+const { courseModel, purchasedModel } = require('../database');
 
 courseRouter.get('/',authMiddleware,userAuthMiddleware,(req,res)=>{
     return res.send("Course Page");
@@ -21,19 +21,54 @@ courseRouter.get('/all',authMiddleware,userAuthMiddleware,async(req,res)=>{
 courseRouter.post('/purchase',authMiddleware,userAuthMiddleware,async(req,res)=>{
     
     const {courseId} = req.body;
-    const selectedCourse = await courseModel.findOne();
+    const userId = req.userId;
+    const selectedCourse = await courseModel.findById(courseId);
+
+    try {
+
+        if(!selectedCourse){
+            return res.json({message : "The Course you are requesting does not exist."});
+        }
+
+        const alreadyPurchased = await purchasedModel.findOne({userId,courseId});
     
-    if(!selectedCourse){
-        return res.json({message : "The Course you are requesting does not exist."});
+        if(alreadyPurchased){
+            return res.json({message : "Course Already Purchased"});
+        }
+    
+        const newPurchase = await purchasedModel.create({
+            userId : userId,
+            courseId : courseId
+        });
+    
+        if (newPurchase){
+            return res.json({message : "Purchase Complete",Course : newPurchase});
+        }
     }
-
-
-
-    return res.json({message : "purchase complete"})
+    catch(error){
+        console.log("Error during purchase  : ",error);
+        return res.status(500).json({message  : "Internal Server Error."});
+    }
 })
 
-courseRouter.get('/purchased',authMiddleware,userAuthMiddleware,(req,res)=>{
-    res.send("purchased courses");
+courseRouter.get('/purchased',authMiddleware,userAuthMiddleware,async(req,res)=>{
+    
+    const userId = req.userId;
+    const purchasedCourses = await purchasedModel.find({userId});
+
+    try{
+        if(purchasedCourses){
+            return res.json({userId : userId,PurchasedCourses : purchasedCourses });
+        }
+        if(!purchasedCourses){
+            return res.json({message : "No Courses purchased yet."});
+        }
+    }
+    catch(error){
+        console.log("Error fetching courses : ",error);
+        return res.json({message:"Error Fetching Courses , Internal server error"});
+    }
+
 })
 
 module.exports = courseRouter;
